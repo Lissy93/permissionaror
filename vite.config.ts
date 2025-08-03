c6ea51a39326eec2e25cffc750852fc1a4087edc
Alicia Sykes <alicia@omg.lol>
2025-08-03 13:09:06 +0100

    ci: Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import marko from "@marko/run/vite";
 import tailwindcss from '@tailwindcss/vite'
+import netlify from '@marko/run-adapter-netlify';
 
 export default defineConfig({
    plugins: [
     tailwindcss(),
-    marko(),
+    marko({
+      adapter: netlify({ edge: true }),
+    }),
   ],
   // ssr: { noExternal: true }
 });
